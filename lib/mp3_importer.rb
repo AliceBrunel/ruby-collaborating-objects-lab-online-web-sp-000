33d60d6e1cbacb422551f17d7915bcda92901b2b
Alice Brunel <alc.brunel@gmail.com>
2019-08-02 20:03:34 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,8 +11,8 @@ class MP3Importer
    @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
   end
 
-  def import(files)
-    files.each {|file| Song.new_by_filename(file)}
+  def import(filenames)
+    self.files.each {|file| Song.new_by_filename(file)}
   end
   
 end
